8d86b2d229f9c16b4e76bf62335151da289488cb
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-06-30 15:44:03 +0530

    admin page created

--- app/(public)/_components/UserDropDown.tsx
+++ app/(public)/_components/UserDropDown.tsx
@@ -18,9 +18,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+import { useSignOut } from "@/hooks/use-signout";
 
 interface iAppProps {
   name: string;
@@ -28,22 +26,10 @@ interface iAppProps {
   image: string;
 }
 
-export function UserDropDown({ name, email, image }: iAppProps) {
-  const router = useRouter();
+export function UserDropDown({ name, email, image }: iAppProps) {  
 
-  async function signOut() {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push("/");
-          toast.success("Signed out successfully");
-        },
-        onError: () => {
-          toast.error("Failed to sign out");
-        },
-      },
-    });
-  }
+  const handleSignOut = useSignOut();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -98,7 +84,7 @@ export function UserDropDown({ name, email, image }: iAppProps) {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={signOut}>
+        <DropdownMenuItem onClick={handleSignOut}>
           <LogOutIcon size={16} className="opacity-60" aria-hidden="true" />
           <span>Logout</span>
         </DropdownMenuItem>
