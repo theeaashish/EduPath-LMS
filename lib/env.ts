36d58dd5320228e672e7c5d5c66f99b69fd8ab49
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-06-30 11:14:30 +0530

    arcjet implemented for bot protection

--- lib/env.ts
+++ lib/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
     AUTH_GITHUB_CLIENT_ID: z.string().min(1),
     AUTH_GITHUB_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
+    ARCJET_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {},
 });
